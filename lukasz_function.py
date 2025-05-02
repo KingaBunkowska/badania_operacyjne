f4b124bb74a203bbcd12a3e48c7fb743e4cad160
LukaszKlon <klon.lukacz@gmail.com>
2025-05-02 02:41:28 +0200

    code changes

--- lukasz_function.py
+++ lukasz_function.py
@@ -5,14 +5,34 @@ from genetic_algorithm import Solution
 
 def remove_task(parent, task):
         for employee in range(len(parent.R)):
-            parent.R[employee][task] = 0
+            if parent.R[employee][task] == 1:
+                parent.R[employee][task] = 0
+                return
+
+def legal_child(child):
+    if not child.is_legal():
+        for employee in range(len(child.R)):
+            time_count = 0
+            task_dict = dict()
+            for task in range(len(child.R[0])):
+                if child.R[employee][task] != 0:
+                    task_dict[task] = child.T[employee][task]
+                    time_count += task_dict[task]
+
+            if time_count > child.L:
+                keys = sorted(task_dict, key=lambda k: task_dict[k], reverse=False)
+                for key in keys:
+                    time_count -= task_dict[key]
+                    child.R[employee][key] = 0
+                    if time_count <= child.L:
+                        break
 
 def breed(population):
 
     def create_child(parent1, parent2):
         child = Solution(deepcopy(parent1.R))
 
-        swap_task_number = random.randint(1,5)
+        swap_task_number = random.randint(1,len(parent1.R[0]))
         employees_number = len(child.R)
 
 
@@ -46,7 +66,7 @@ def breed(population):
 def mutation(children):
     children_number = len(children)
 
-    mutation_number = random.randrange(0, children_number)
+    mutation_number = random.randrange(1, len(children[0].R[0]))
     employee_numbers = len(children[0].R)
     task_numbers = len(children[0].R[0])
 
@@ -59,13 +79,16 @@ def mutation(children):
         remove_task(children[child_number], task)
         children[child_number].R[employee][task] = 1
 
+    for child_number in range(children_number):
+        legal_child(children[child_number])
+
     return children
 
 def select(population, children):
     n = len(population)
     all = population + children
 
-    sorted(all, key = lambda obj: obj.f)
+    all.sort( key = lambda obj: obj.f)
 
     return all[:n]
 
